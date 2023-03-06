953db5bac8cf397911a7bade6df1894d70dbe965
asocial <roman.chernikov@madcap.one>
2023-03-06 12:12:28 +0100

    ✨ introduce distance calculation between vectors
    - create interface Distance.kt with 2 implementations
      - ManhattanDistance
      - EuclideanDistance
    - add VecCloud.kt with VecCloud.closestDistance(IntVec, Distance)

---
+++ src/main/kotlin/edu/tum/romance/whatsthis/math/Distance.kt
@@ -0,0 +1,30 @@
+package edu.tum.romance.whatsthis.math
+
+import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+interface Distance {
+    fun distance(a: IntVec, b: IntVec): Double
+}
+
+object EuclideanDistance: Distance {
+    override fun distance(a: IntVec, b: IntVec): Double {
+        var sum = 0.0
+        for(i in a.data.indices) {
+            sum += (a.data[i] - b.data[i]).toDouble().pow(2.0)
+        }
+        return sqrt(sum)
+    }
+}
+
+@Suppress("unused")
+object ManhattanDistance: Distance {
+    override fun distance(a: IntVec, b: IntVec): Double {
+        var sum = 0.0
+        for(i in a.data.indices) {
+            sum += abs(a.data[i] - b.data[i])
+        }
+        return sum
+    }
+}
---
+++ src/main/kotlin/edu/tum/romance/whatsthis/math/VecCloud.kt
@@ -0,0 +1,18 @@
+@file:Suppress("unused")
+
+package edu.tum.romance.whatsthis.math
+
+class VecCloud(c: List<IntVec>){
+    var cloud: List<IntVec> = c
+
+    fun closestDistance(to: IntVec, f: Distance = EuclideanDistance): Double {
+        var min = Double.MAX_VALUE
+        for(i in cloud.indices) {
+            val dist = f.distance(cloud[i], to)
+            if(dist < min) {
+                min = dist
+            }
+        }
+        return min
+    }
+}
