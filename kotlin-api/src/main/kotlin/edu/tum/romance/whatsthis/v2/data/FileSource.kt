c71a7413d01bac2090840d451ace413cd8598c27
Roman Shchekotov (Doomie) <rom.shchekotov@gmail.com>
2023-03-24 18:39:43 +0100

    ✅ provide full data coverage
    Added more tests to (almost) fully cover all different cases for data loading.
    
    The only missing cases are OS dependent and thus not 'testable' in a single run (that aside, the macOS-part still crashes due to uncertainty whether the OS query for it is correct (perhaps it's Darwin instead of Mac).

--- kotlin-api/src/main/kotlin/edu/tum/romance/whatsthis/v2/data/FileSource.kt
+++ kotlin-api/src/main/kotlin/edu/tum/romance/whatsthis/v2/data/FileSource.kt
@@ -3,9 +3,6 @@ package edu.tum.romance.whatsthis.v2.data
 import java.io.File
 
 class FileSource(file: File): DataSource<File>(file) {
-    @Suppress("unused")
-    constructor(path: String): this(File(path))
-
     override fun load(): Pair<String, String> {
         return when(source.extension) {
             "html" -> parseHTML(source.readText()).let { it.first to (it.second ?: source.nameWithoutExtension) }
